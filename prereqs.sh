29436dbb4229bda41312bb11073ae1a2897fd61b
Aaron Mininger <mininger@umich.edu>
2014-09-16 11:43:16 -0400

    Added support info

---
+++ prereqs.sh
@@ -0,0 +1,9 @@
+# LCM and APRIL
+sudo apt-get install build-essential git-core ant subversion gtk-doc-tools libglib2.0-dev libusb-1.0-0-dev gv libncurses-dev openjdk-6-jdk autopoint libgl1-mesa-dev libpng12-dev libdc1394-22-dev
+
+# LIBFREENECT
+sudo apt-get install git-core cmake freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev
+
+# SOAR
+sudo apt-get install openjdk-6-jdk swig build-essential python-all-dev scons
+
---
+++ setup.sh
@@ -0,0 +1,27 @@
+git submodule update
+
+# LCM
+cd $LCM_HOME
+./bootstrap
+./configure
+make
+sudo make install
+
+# APRIL
+cd $APRIL_HOME/src
+make
+cd $APRIL_HOME/java
+ant
+
+# LIBFREENECT
+cd $ROSIE_PROJ/libfreenect
+mkdir build
+cd build
+cmake ..
+make 
+sudo make install
+sudo ldconfig /usr/local/lib64/
+
+# SOAR
+cd $ROSIE_PROJ/soar
+scons
